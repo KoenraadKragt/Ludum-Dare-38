1f9cf38f8e297e4a03db1b493635f13d8de853cf
Mark de Wit <mark_de_wit_95@hotmail.com>
2017-04-22 16:12:55 +0200

    Spaceship hitpoints and 2D colliders

---
+++ Assets/Scripts/BulletDealDamage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDealDamage : MonoBehaviour {
+
+    public int damage;
+
+	void OnCollisionEnter2D(Collision2D other)
+    {
+        other.gameObject.SendMessage("TakeDamage",damage);
+    }
+}
