18af962e78367fe2efccc6670144d98611b39dfb
Koenraad <koenraad.kragt@gmail.com>
2017-04-22 12:44:58 +0200

    Aim & Shoot

---
+++ Assets/Scripts/AimTurret.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimTurret : MonoBehaviour {
+
+    GameObject crosshair;
+
+    void Start()
+    {
+        crosshair = GameObject.FindGameObjectWithTag("CrossHair");
+
+    }
+
+	void Update () {
+        transform.LookAt(crosshair.transform.position);
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed = 5;
+    public float destroyTime = 5;
+
+    void Start()
+    {
+        Destroy(this.gameObject, destroyTime);
+
+    }
+	
+	void Update () {
+        transform.position += transform.forward * Time.deltaTime * speed;
+	}
+}
