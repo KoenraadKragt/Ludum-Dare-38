66e4e4db435e4a91d0afa6d1e7de7ac8b4f20860
Koenraad <koenraad.kragt@gmail.com>
2017-04-22 21:44:41 +0200

    Max doe het lekker zelf

---
+++ Assets/Scripts/ColorChangeOnDamage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeOnDamage : MonoBehaviour
+{
+    public Renderer planetGround;
+    private Hitpoints hitpoints;
+    private int curHP;
+    private int maxHP;
+
+    void Start()
+    {
+        hitpoints = GetComponent<Hitpoints>();
+        curHP = hitpoints.hitpoints;
+        maxHP = hitpoints.hitpoints;
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+
+        float amount = Mathf.Lerp(curHP, hitpoints.hitpoints, 1 * Time.deltaTime) / maxHP;
+
+        planetGround.material.SetFloat("_SSSVertex_Color_aIntensety", amount);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            this.gameObject.SendMessage("TakeDamage", 1, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
