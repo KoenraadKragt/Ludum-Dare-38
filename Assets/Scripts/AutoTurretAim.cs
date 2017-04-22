cb7a60d58a87db12b16cf3b207bf8f153dcedbc6
Mark de Wit <mark_de_wit_95@hotmail.com>
2017-04-23 00:45:52 +0200

    Auto Turret

---
+++ Assets/Scripts/AutoTurretAim.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoTurretAim : MonoBehaviour {
+
+    [HideInInspector]
+    public bool canShoot = true;
+
+    private GameObject target;
+
+    private GameObject crosshair;
+    private Transform turretAnchor;
+
+    void Start()
+    {
+        turretAnchor = transform.parent;
+
+    }
+
+    void Update()
+    {
+        Quaternion targetRot;
+
+        GameObject[] temp = GameObject.FindGameObjectsWithTag("Enemy");
+        
+        if (target != null)
+        {
+            targetRot = Quaternion.LookRotation(target.transform.position);
+            transform.rotation = targetRot;
+        }
+
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        this.target = target;
+    }
+
+    public void ClearTarget()
+    {
+        this.target = null;
+    }
+
+    public bool HasTarget()
+    {
+        if(target != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
