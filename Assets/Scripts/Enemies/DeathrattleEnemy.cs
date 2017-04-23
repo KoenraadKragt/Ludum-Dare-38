6dffafb5519256f9f0387b4430d56d1c6f5daf8b
Mark de Wit <mark_de_wit_95@hotmail.com>
2017-04-23 23:14:54 +0200

    Added enemy.

---
+++ Assets/Scripts/Enemies/DeathrattleEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathrattleEnemy : MonoBehaviour {
+
+    public GameObject spawnling;
+    public int amount;
+
+    public void Death()
+    {
+        for(int i = 0; i < amount; i++)
+        {
+            GameObject newEnemy = Instantiate(spawnling, this.gameObject.transform.position, this.transform.rotation);
+            GameObject.FindGameObjectWithTag("Manager").GetComponent<WaveManager>().IncreaseEnemyCount();
+        }
+    }
+}
