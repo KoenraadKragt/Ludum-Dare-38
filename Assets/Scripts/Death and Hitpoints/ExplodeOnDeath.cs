cc402b4fe0bb2d912a48915b42a3cb2afa0928dc
Koenraad <koenraad.kragt@gmail.com>
2017-04-24 14:33:54 +0200

    Explosions

---
+++ Assets/Scripts/Death and Hitpoints/ExplodeOnDeath.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeOnDeath : MonoBehaviour {
+
+    public GameObject explosion;
+
+	public void Death()
+    {
+        GameObject.Instantiate(explosion, transform.position, transform.rotation);
+    }
+}
