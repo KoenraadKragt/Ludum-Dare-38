cbc78e1e13464ad2348135ed16ebd86f8104b8e3
Mark de Wit <mark_de_wit_95@hotmail.com>
2017-04-24 13:37:01 +0200

    New ships and stuff

---
+++ Assets/Scripts/Enemies/EnemyShooting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour {
+
+    public GameObject projectile;
+    private float nextFire = 2.5F;
+    private float myTime = 0.0F;
+    public float fireDelta = 2.5F;
+
+
+    void Update()
+    {
+        myTime = myTime + Time.deltaTime;
+
+        if (myTime > nextFire)
+        {
+            nextFire = myTime + fireDelta;
+            GameObject newEnemy = Instantiate(projectile, this.gameObject.transform.position, this.transform.rotation);
+            GameObject.FindGameObjectWithTag("Manager").GetComponent<WaveManager>().IncreaseEnemyCount();
+        }
+    }
+}
