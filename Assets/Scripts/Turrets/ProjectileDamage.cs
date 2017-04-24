528e8932dcdffbb9ca8b1a30e44b0eda2085a888
Koenraad <koenraad.kragt@gmail.com>
2017-04-24 18:20:44 +0200

    RocketExplosion

--- Assets/Scripts/Turrets/ProjectileDamage.cs
+++ Assets/Scripts/Turrets/ProjectileDamage.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class ProjectileDamage : PoolObject {
 
     public int damage;
+    public GameObject explosion;
 
 	void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.tag == "Enemy")
         {
             other.gameObject.SendMessage("TakeDamage", damage);
+            if(explosion != null)
+            {
+                GameObject.Instantiate(explosion, transform.position, transform.rotation);
+            }
             Destroy();
         }
     }
