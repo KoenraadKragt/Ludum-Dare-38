74d9a9d1f8d609f173a9b1dee4f1fdd659508774
Koenraad <koenraad.kragt@gmail.com>
2017-04-24 12:35:44 +0200

    Starting Game (WIP)

---
+++ Assets/Scripts/StartGameUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameUI : MonoBehaviour {
+
+    public GameObject shopUI;
+    public GameObject startGameUI;
+
+	public void StartGame()
+    {
+        startGameUI.SetActive(false);
+        shopUI.SetActive(true);
+    }
+}
