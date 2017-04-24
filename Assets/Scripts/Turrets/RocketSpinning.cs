491b958a2255fd87e6acc379a89e45e5e35e38dd
Koenraad <koenraad.kragt@gmail.com>
2017-04-24 17:52:26 +0200

    Homing Rockets

---
+++ Assets/Scripts/Turrets/RocketSpinning.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketSpinning : MonoBehaviour {
+
+    public float rotationSpeed;
+
+	void Update () {
+        transform.RotateAround(transform.forward, rotationSpeed * Time.deltaTime);
+	}
+}
