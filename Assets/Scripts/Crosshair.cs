e7eef30e910f31aa5e740f0ca9eff05dd05d7472
Koenraad <koenraad.kragt@gmail.com>
2017-04-24 21:17:54 +0200

    Crosshair update

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Crosshair : MonoBehaviour {
 
+    [Range(-10,10)]
+    public float depth;
 
 	void Update () {
 
         Vector3 v3 = Input.mousePosition;
-        v3.z = 10.0f;
+        v3.z = depth;
         v3 = Camera.main.ScreenToWorldPoint(v3);
         transform.position = v3;
 	}
